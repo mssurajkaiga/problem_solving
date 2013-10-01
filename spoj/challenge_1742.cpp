3cbc4feb2e3455e0df88e5fb284dc0e49dc20e5b
M S Suraj <mssurajkaiga@gmail.com>
2013-10-02 04:14:57 +0530

    Reduced to 619 bytes, 107 bytes more to go

--- spoj/challenge_1742.cpp
+++ spoj/challenge_1742.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-using namespace std;string c(""),l(0,32768);int k=0;void e(){cout<<"COMPILE ERROR";exit(0);}int run(int s,int v){int j;for(int i=s;i<v;i++)switch(c[i]){case'>':++k%=32768;break;case'<':if(--k<0)k=32767;break;case'+':if(++l[k]>255)l[k]=0;break;case'-':if(--l[k]<0)l[k]=255;break;case'.':cout<<l[k];break;case'[':for(j=i+1;c[j]!=']';j++);if(l[k]>0)run(i--+1,j);else i=j;break;case']':e();}}int main(){string b;int o=0,x=0,i;while(cin){b="";getline(cin,b);for(i=0;i<b.size();i++){if(b[i]=='%')b.resize(i);else b[i]=='['?o++:b[i]==']'?x++:0;}c.append(b);}if(o!=x)e();run(0,c.size());}
+#define z break;
+#define r case
+using namespace std;string c(""),l(0,32768);int k=0;int e(){cout<<"COMPILE ERROR";exit(0);}int f(int s,int v){int j;for(int i=s;i<v;i++)switch(c[i]){r'>':++k%=32768;z r'<':if(--k<0)k=32767;z r'+':if(++l[k]>255)l[k]=0;z r'-':if(--l[k]<0)l[k]=255;z r'.':cout<<l[k];z r'[':for(j=i+1;c[j]!=']';j++);if(l[k]>0)f(i--+1,j);else i=j;z r']':e();}}int main(){string b;int o=0,x=0,i;while(cin){b="";getline(cin,b);for(i=0;i<b.size();i++){if(b[i]=='%')b.resize(i);else b[i]=='['?o++:b[i]==']'?x++:0;}c.append(b);}if(o!=x)e();f(0,c.size());}
