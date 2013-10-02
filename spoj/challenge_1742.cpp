d0dd138dcab5dc22f0c2db98dae731b265ce9606
M S Suraj <mssurajkaiga@gmail.com>
2013-10-02 17:59:01 +0530

    Removed stupidity, now works on nested loops!!, this a non-compressed code

--- spoj/challenge_1742.cpp
+++ spoj/challenge_1742.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-#define c case
-#define v int
-#define b break;
-using namespace std;string x(""),m(0,32768);v k=0;v e(){cout<<"COMPILE ERROR";exit(0);}v r(v s,v a){v j;for(v i=s;i<a;i++)switch(x[i]){c'>':++k%=32768;b c'<':if(--k<0)k=32767;b c'+':if(++m[k]>255)m[k]=0;b c'-':if(--m[k]<0)m[k]=255;b c'.':cout<<m[k];b c'[':for(j=i+1;x[j]!=']';j++);if(m[k]>0)r(i--+1,j);else i=j;b c']':e();}}v main(){string t;v o=0,i=0;while(getline(cin,t)){for(i=0;char l=t[i];i++)l==91?o++:l==93?o--:l==37?t[i--]=0:0;x.append(t,0,i);}if(o)e();r(0,x.size());}
+using namespace std;
+
+string code(""), mem(0, 32768);
+int k=0, M=32768;
+char l;
+void e() {cout<<"COMPILE ERROR"; exit(0);}
+
+void run(int start){
+	for(int i=start; l=code[i]; i++) 
+			switch(code[i]) {
+				case '>':
+					++k%=M;
+					break;
+
+				case '<':
+					if(--k<0) k=M-1;
+					break;
+
+				case '+':
+					if (++mem[k]>255) mem[k]=0;
+					break;
+
+				case '-':
+					if (--mem[k]<0) mem[k]=255;
+					break;
+
+				case '.':
+					cout<<mem[k];
+					break;
+
+				case '[':
+					if (mem[k]>0) {run(i--+1);}
+					else {
+						for(int o=1;o&&code[i++];) {
+							if(code[i]=='[') o++;
+							if (code[i]==']') o--;
+						}
+					}
+					break;
+				case ']': return;
+				}
+			
+}
+
+int main() {
+	string temp;
+	int open_loop = 0,i=0;
+	while(getline(cin, temp)) {
+		for (i=0; l=temp[i]; i++) {
+			l=='[' ? open_loop++ : l==']' ? open_loop-- : l=='%' ?  temp[i--]=0 : 0;
+		}
+		code.append(temp,0,	i);
+	}
+	if (open_loop) {
+		e();
+	}
+
+	run(0);
+	return 0;
+}
