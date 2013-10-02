c302ef50bf613bc8f8cdcd2a55c95f9a1a01ec18
M S Suraj <mssurajkaiga@gmail.com>
2013-10-02 18:35:28 +0530

    Compressed correct code to 580 bytes, 68 more to go

--- spoj/challenge_1742.cpp
+++ spoj/challenge_1742.cpp
@@ -1,63 +1,6 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-using namespace std;
-
-string code(""), mem(0, 32768);
-int k=0, M=32768;
-char l;
-void e() {cout<<"COMPILE ERROR"; exit(0);}
-
-void run(int start){
-	for(int i=start; l=code[i]; i++) 
-			switch(code[i]) {
-				case '>':
-					++k%=M;
-					break;
-
-				case '<':
-					if(--k<0) k=M-1;
-					break;
-
-				case '+':
-					if (++mem[k]>255) mem[k]=0;
-					break;
-
-				case '-':
-					if (--mem[k]<0) mem[k]=255;
-					break;
-
-				case '.':
-					cout<<mem[k];
-					break;
-
-				case '[':
-					if (mem[k]>0) {run(i--+1);}
-					else {
-						for(int o=1;o&&code[i++];) {
-							if(code[i]=='[') o++;
-							if (code[i]==']') o--;
-						}
-					}
-					break;
-				case ']': return;
-				}
-			
-}
-
-int main() {
-	string temp;
-	int open_loop = 0,i=0;
-	while(getline(cin, temp)) {
-		for (i=0; l=temp[i]; i++) {
-			l=='[' ? open_loop++ : l==']' ? open_loop-- : l=='%' ?  temp[i--]=0 : 0;
-		}
-		code.append(temp,0,	i);
-	}
-	if (open_loop) {
-		e();
-	}
-
-	run(0);
-	return 0;
-}
+#define C case
+#define B break;
+using namespace std;int k=0,M=32768;string c(""),m(0,M),t;char l;int e(){cout<<"COMPILE ERROR";exit(0);}void r(int s){for(int i=s;l=c[i];i++)switch(c[i]){C'>':++k%=M;B C'<':--k<0?k=M-1:0;B C'+':++m[k]>255?m[k]=0:0;B C'-':--m[k]<0?m[k]=255:0;B C'.':cout<<m[k];B C'[':if(m[k]>0)r(i--+1);else for(int o=1;o&&c[i++];)c[i]==91?o++:c[i]==93?o--:0;B C']':return;}}int main(){int o=0,i=0;while(getline(cin,t)){for(i=0;l=t[i];i++)l==91?o++:l==93?o--:l==37?t[i--]=0:0;c.append(t,0,i);}if(o)e();r(0);}
