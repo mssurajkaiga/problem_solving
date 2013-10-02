8b22fce79abc360c06d8b61a4726e14fbc73888a
M S Suraj <mssurajkaiga@gmail.com>
2013-10-02 20:54:31 +0530

    Reduced size to 535 bytes, 23 more to go!

--- spoj/challenge_1742.cpp
+++ spoj/challenge_1742.cpp
@@ -3,4 +3,5 @@
 #include <stdlib.h>
 #define C case
 #define B break;
-using namespace std;int k=0,M=32768;string c(""),m(0,M),t;char l;int e(){cout<<"COMPILE ERROR";exit(0);}void r(int s){for(int i=s;l=c[i];i++)switch(c[i]){C'>':++k%=M;B C'<':--k<0?k=M-1:0;B C'+':++m[k]>255?m[k]=0:0;B C'-':--m[k]<0?m[k]=255:0;B C'.':cout<<m[k];B C'[':if(m[k]>0)r(i--+1);else for(int o=1;o&&c[i++];)c[i]==91?o++:c[i]==93?o--:0;B C']':return;}}int main(){int o=0,i=0;while(getline(cin,t)){for(i=0;l=t[i];i++)l==91?o++:l==93?o--:l==37?t[i--]=0:0;c.append(t,0,i);}if(o)e();r(0);}
+#define y m[k]
+using namespace std;int k=0,M=32768;string c,m(M,0),t;char l;void r(int s){for(int i=s;l=c[i];i++)switch(l){C'>':++k%=M;B C'<':--k<0?k=M-1:0;B C'+':++y>255?y=0:0;B C'-':--y<0?y=255:0;B C'.':cout<<y;B C'[':if(y>0)r(i--+1);else for(int o=1;o&&c[i++];)c[i]==91?o++:c[i]==93?o--:0;B C']':return;}}main(){int o=0,i;while(cin>>t){for(i=0;l=t[i];i++)l==91?o++:l==93?o--:l==37?t[i--]=0:0;c+=t;}if(o){cout<<"COMPILE ERROR";exit(0);};r(0);}
